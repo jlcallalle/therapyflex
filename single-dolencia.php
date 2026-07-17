7e79606cd90b52b6ffbb25446a9a8f4435cd75ac
Jorge <vasslatam01@invex.com>
2026-07-17 03:31:17 -0500

    agrega imagen en simgle dolencia

--- single-dolencia.php
+++ single-dolencia.php
@@ -3,13 +3,13 @@
 <main id="contenido-principal" class="container mt-5">
   <h1><?php echo esc_html(get_the_title()); ?></h1>
 
-  <?php if (has_post_thumbnail()) : ?>
-    <div class="mb-4">
-      <?php the_post_thumbnail('large', ['class' => 'img-fluid']); ?>
-    </div>
-  <?php endif; ?>
+  <div class="contenido contenido-dolencia">
+    <?php if (has_post_thumbnail()) : ?>
+      <figure class="dolencia-featured-image">
+        <?php the_post_thumbnail('medium', array('class' => 'img-fluid')); ?>
+      </figure>
+    <?php endif; ?>
 
-  <div class="contenido">
     <?php the_content(); ?>
   </div>
 </main>
